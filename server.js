34e10d22c3937a6b317726e65126770a9fa2b4d1
Sandy <sandycchen30@gmail.com>
2022-06-06 02:27:40 -0700

    updated code

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useUnifiedTopology: true
 });
 
 app.use(require("./routes/api.js"));
